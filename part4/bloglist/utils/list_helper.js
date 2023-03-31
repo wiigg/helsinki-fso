0668654a2cdb846a910d8594e5eaa88e63e92d1a
wiigg <danny@wigg.uk>
2023-03-31 20:59:50 +0100

    Exercises 4.3-4.5

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,22 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favouriteBlog = (blogs) => {
+  return blogs.reduce((favourite, blog) => {
+    if (favourite.likes < blog.likes) {
+      return blog;
+    }
+    return favourite;
+  }, blogs[0] || {});
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favouriteBlog,
+};
