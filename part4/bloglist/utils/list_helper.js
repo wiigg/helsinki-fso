3b1ebc9b6c8bd62890b02c16b5bd18554c31cdd6
wiigg <danny@wigg.uk>
2023-04-03 19:28:54 +0100

    Exercises 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -15,8 +17,18 @@ const favouriteBlog = (blogs) => {
   }, blogs[0] || {});
 };
 
+const mostBlogs = (blogs) => {
+  const authors = _.countBy(blogs, "author");
+  const maxAuthor = _.maxBy(Object.keys(authors), (author) => authors[author]);
+  return {
+    author: maxAuthor,
+    blogs: authors[maxAuthor],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
+  mostBlogs,
 };
