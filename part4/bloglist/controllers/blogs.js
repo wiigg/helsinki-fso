91eaad0ddca02ae6b05220e13cfb72eb2808855b
wiigg <danny@wigg.uk>
2023-04-23 15:08:18 +0100

    Exercises 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,17 +1,34 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  if (request.body.title === undefined || request.body.url === undefined) {
+  const { title, author, url, likes, userId } = request.body;
+
+  if (title === undefined || url === undefined) {
     return response.status(400).end();
   }
-  const blog = new Blog(request.body);
+
+  const users = await User.find({});
+  const user = users[0];
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: likes || 0,
+    user: user.id,
+  });
+
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog.id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
