e46848fffed896774582d4b599f9daf39085148a
wiigg <danny@wigg.uk>
2023-04-25 21:15:13 +0100

    Exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 
 const mongoUrl = config.MONGODB_URI;
@@ -15,5 +16,6 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
